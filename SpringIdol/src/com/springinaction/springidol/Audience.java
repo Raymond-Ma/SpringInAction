c6189c09296f7160bb5fc6ce4b6ee877ea1574c0
RaymondMa <raymond_ma@aliyun.com>
2015-12-24 08:52:27 +0800

    Add_Audience_As_Aop_Into_SpringIdol
    Add audience as aop into springidol, define and call audience's method
    at each aop pointcut

---
+++ SpringIdol/src/com/springinaction/springidol/Audience.java
@@ -0,0 +1,25 @@
+package com.springinaction.springidol;
+
+/**
+ * This is an audience on enjoying the Spring Idol
+ * @author RaymondMa
+ * @version 0.0.1
+ */
+public class Audience {
+	
+	public void takeSeats() {
+		System.out.println("The audience is taking their seats");
+	}
+	
+	public void turnOffCellPhones() {
+		System.out.println("The audience is turning off their cellphones");
+	}
+	
+	public void applaud() {
+		System.out.println("CLAP CLAP CLAP");
+	}
+	
+	public void demandRefund() {
+		System.out.println("Boo! We want our money back!");
+	}
+}
