b7770f90f8b09f8c518a4006e34a0b6e7ec86391
RaymondMa <raymond_ma@aliyun.com>
2015-12-27 10:51:22 +0800

    Use_Aop_Annotation_With_Arguments_In_Springidol
    Mask aop advice on class magician, and define aop annotation with
    arguments catched in class thinker

--- SpringIdol/src/com/springinaction/wizard/Magician.java
+++ SpringIdol/src/com/springinaction/wizard/Magician.java
@@ -1,15 +1,23 @@
 package com.springinaction.wizard;
 
 import com.springinaction.springidol.MindReader;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * This is a magician, who would read thoughts from thinker in Spring Idol contest
  * @author RaymondMa 
  * @version 0.0.1
  */
+@Aspect
 public class Magician implements MindReader {
 	
+	@Pointcut("execution(* com.springinaction.springidol.Thinker.thinkOfSomething(String)) && args(thoughts)")
+	public void thinking(String thoughts){}
+	
 	@Override
+	@Before("thinking(thoughts)")
 	public void interceptThoughts(String thoughts) {
 		System.out.println("Intercepting volunteer's thoughts : " + thoughts);
 	}
